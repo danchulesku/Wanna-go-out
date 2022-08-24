4dcc99f59c3bf31480090589370488daae60f6b7
Daniil Grishchenko <grishchenko_daniil_1@mail.ru>
2022-08-24 16:50:07 +0300

    icon log-in

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,6 +11,7 @@ import "channels"
 import 'bootstrap/dist/js/bootstrap'
 import 'bootstrap/dist/css/bootstrap'
 import 'stylesheets/application'
+import "@fortawesome/fontawesome-free/css/all"
 
 Rails.start()
 Turbolinks.start()
