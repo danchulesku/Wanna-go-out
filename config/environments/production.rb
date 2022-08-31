6c227dad80eecef3de67024717156c1fb8c89e95
Daniil Grishchenko <grishchenko_daniil_1@mail.ru>
2022-08-31 20:01:40 +0300

    config fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,6 +11,8 @@ Rails.application.configure do
   # and those relying on copy on write to perform better.
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
+
+  config.action_mailer.default_url_options = { :host => "https://bbq516.herokuapp.com/" }
   config.action_mailer.delivery_method = :mailjet
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
