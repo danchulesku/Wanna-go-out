a5289d43698b8f5645bdfd97f6b55decabd3fccd
Daniil Grishchenko <grishchenko_daniil_1@mail.ru>
2022-09-01 03:59:43 +0300

    fix

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -42,7 +42,6 @@ class CommentsController < ApplicationController
   def notify_subscribers(event, comment)
     # Собираем всех подписчиков и автора события в массив мэйлов, исключаем повторяющиеся
     all_emails = (event.subscriptions.map(&:user_email) + [event.user.email]).uniq - [current_user.email]
-    debugger
     # По адресам из этого массива делаем рассылку
     # Как и в подписках, берём EventMailer и его метод comment с параметрами
     # И отсылаем в том же потоке
