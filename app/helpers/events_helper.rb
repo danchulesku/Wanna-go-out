c7f03ca6d4f4b934f2be46322bdf9c4a9a1e41b4
Daniil Grishchenko <grishchenko_daniil_1@mail.ru>
2022-09-04 17:41:04 +0300

    SSL problem

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,6 +1,5 @@
 module EventsHelper
   def event_photo(photo)
-    link_to (image_tag photo.variant(resize_to_fit: [600, 600]), class: "img-fluid mt-2"),
-            rails_blob_url(photo)
+    link_to (image_tag photo.variant(resize_to_fit: [500, 500]), class: "img-fluid mt-2"), rails_blob_url(photo)
   end
 end
