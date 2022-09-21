b749e39f7a75e2798068010859dc6cf2ae832b74
daniil516 <grishchenko_daniil_1@mail.ru>
2022-09-21 13:18:18 +0000

    production fix

--- db/schema.rb
+++ db/schema.rb
@@ -12,14 +12,11 @@
 
 ActiveRecord::Schema.define(version: 2022_09_01_180234) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
-    t.bigint "record_id", null: false
-    t.bigint "blob_id", null: false
+    t.integer "record_id", null: false
+    t.integer "blob_id", null: false
     t.datetime "created_at", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
@@ -38,7 +35,7 @@ ActiveRecord::Schema.define(version: 2022_09_01_180234) do
   end
 
   create_table "active_storage_variant_records", force: :cascade do |t|
-    t.bigint "blob_id", null: false
+    t.integer "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
@@ -46,8 +43,8 @@ ActiveRecord::Schema.define(version: 2022_09_01_180234) do
   create_table "comments", force: :cascade do |t|
     t.text "body"
     t.string "user_name"
-    t.bigint "event_id", null: false
-    t.bigint "user_id"
+    t.integer "event_id", null: false
+    t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["event_id"], name: "index_comments_on_event_id"
@@ -61,14 +58,14 @@ ActiveRecord::Schema.define(version: 2022_09_01_180234) do
     t.datetime "datetime"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.bigint "user_id", null: false
+    t.integer "user_id", null: false
     t.string "pincode"
     t.index ["user_id"], name: "index_events_on_user_id"
   end
 
   create_table "photos", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.bigint "event_id", null: false
+    t.integer "user_id", null: false
+    t.integer "event_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["event_id"], name: "index_photos_on_event_id"
@@ -78,8 +75,8 @@ ActiveRecord::Schema.define(version: 2022_09_01_180234) do
   create_table "subscriptions", force: :cascade do |t|
     t.string "user_name"
     t.string "user_email"
-    t.bigint "event_id", null: false
-    t.bigint "user_id"
+    t.integer "event_id", null: false
+    t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["event_id"], name: "index_subscriptions_on_event_id"
